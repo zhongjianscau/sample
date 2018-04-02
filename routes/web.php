37d3f31844625d659dcdbecf5addd7126b9e75e6
vagrant <vagrant@homestead>
2018-04-02 01:21:34 +0000

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@
 // });
 
 Route::get('/', 'StaticPagesController@home');
-Route::get('/', 'StaticPagesController@help');
-Route::get('/', 'StaticPagesController@about');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
